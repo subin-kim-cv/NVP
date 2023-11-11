ed1122de6f251dae09f54588cd3ecdca1c04f93e
jakobtroidl <jtroidl@g.harvard.edu>
2023-11-11 14:16:04 -0500

    add visualization script

---
+++ visualization/ng_vis.py
@@ -0,0 +1,59 @@
+from __future__ import print_function
+
+import argparse
+import numpy as np
+
+import neuroglancer
+import neuroglancer.cli
+import webbrowser
+
+
+def add_example_layers(state):
+    a = np.load(
+        "/home/jakobtroidl/Desktop/NVP/outputs/reconstructed-nvp.npy"
+    )
+
+    b = a < 0.08160067
+    b = b.astype(np.uint8) * 255
+
+    dimensions = neuroglancer.CoordinateSpace(
+        names=["x", "y", "z"], units="nm", scales=[1, 1, 1]
+    )
+
+    state.dimensions = dimensions
+    state.layers.append(
+        name="a",
+        layer=neuroglancer.LocalVolume(
+            data=a,
+            dimensions=dimensions,
+            voxel_offset=(1, 1, 1),
+        ),
+        shader="""
+void main() {
+  emitRGB(vec3(toNormalized(getDataValue(0)),
+               toNormalized(getDataValue(1)),
+               toNormalized(getDataValue(2))));
+}
+""",
+    )
+    state.layers.append(
+        name="b",
+        layer=neuroglancer.LocalVolume(
+            data=b,
+            dimensions=dimensions,
+            volume_type="image",
+        ),
+    )
+    return a, b
+
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    neuroglancer.cli.add_server_arguments(ap)
+    args = ap.parse_args()
+    neuroglancer.cli.handle_server_arguments(args)
+    viewer = neuroglancer.Viewer()
+    with viewer.txn() as s:
+        a, b = add_example_layers(s)
+
+    webbrowser.open_new(viewer.get_viewer_url())
