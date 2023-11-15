ab54e9da4375cb7f7e57b1f2c6b88f2ef79b5545
jakobtroidl <jtroidl@g.harvard.edu>
2023-11-14 22:48:39 -0500

    test interpolating rather than feature unfolding

--- sparsegrid.py
+++ sparsegrid.py
@@ -41,34 +41,46 @@ class SparseGrid(nn.Module):
             y_res = self.y_resolution
             tmp_embeddings = self.embeddings
 
+        tmp_embeddings = tmp_embeddings.permute(3, 0, 1, 2).unsqueeze(0)
+        inputs = inputs.unsqueeze(0).unsqueeze(0).unsqueeze(0)
+        grid_features = torch.nn.functional.grid_sample(tmp_embeddings, inputs, mode='bilinear', padding_mode='reflection')
+        grid_features = grid_features.squeeze()
+        grid_features = grid_features.permute(1, 0)
+
+        
+
+        
+
+
+
         # round down
-        t_coord = inputs[:, 0]
-        t_coord_float = ((t_res-1)*t_coord)
-        t_coord_idx = (t_coord_float+0.5).type(torch.int64)
-        t_coord_idx = torch.clamp(t_coord_idx, 0, t_res-1)
+        # t_coord = inputs[:, 0]
+        # t_coord_float = ((t_res-1)*t_coord)
+        # t_coord_idx = (t_coord_float+0.5).type(torch.int64)
+        # t_coord_idx = torch.clamp(t_coord_idx, 0, t_res-1)
 
-        x_coord = inputs[:, 1]
-        x_coord_float = ((x_res-1)*x_coord)
-        x_coord_idx = (x_coord_float+0.5).type(torch.int64)
-        x_coord_idx = torch.clamp(x_coord_idx, 0, x_res-1)
+        # x_coord = inputs[:, 1]
+        # x_coord_float = ((x_res-1)*x_coord)
+        # x_coord_idx = (x_coord_float+0.5).type(torch.int64)
+        # x_coord_idx = torch.clamp(x_coord_idx, 0, x_res-1)
 
-        y_coord = inputs[:, 2]
-        y_coord_float = ((y_res-1)*y_coord)
-        y_coord_idx = (y_coord_float+0.5).type(torch.int64)
-        y_coord_idx = torch.clamp(y_coord_idx, 0, y_res-1)
+        # y_coord = inputs[:, 2]
+        # y_coord_float = ((y_res-1)*y_coord)
+        # y_coord_idx = (y_coord_float+0.5).type(torch.int64)
+        # y_coord_idx = torch.clamp(y_coord_idx, 0, y_res-1)
 
-        grid_features = None
-        unfold_list = [-1, 0, 1]
+        # grid_features = None
+        # unfold_list = [-1, 0, 1]
 
-        for i in unfold_list:
-            for j in unfold_list:
-                vx = torch.clamp(x_coord_idx+i, 0, x_res-1)
-                vy = torch.clamp(y_coord_idx+j, 0, y_res-1)
-                feat = (tmp_embeddings[t_coord_idx, vx, vy, :])
-                if grid_features == None:
-                    grid_features = feat
-                else:
-                    grid_features = torch.cat((grid_features, feat), dim=1)
+        # for i in unfold_list:
+        #     for j in unfold_list:
+        #         vx = torch.clamp(x_coord_idx+i, 0, x_res-1)
+        #         vy = torch.clamp(y_coord_idx+j, 0, y_res-1)
+        #         feat = (tmp_embeddings[t_coord_idx, vx, vy, :])
+        #         if grid_features == None:
+        #             grid_features = feat
+        #         else:
+        #             grid_features = torch.cat((grid_features, feat), dim=1)
 
 
         return grid_features
