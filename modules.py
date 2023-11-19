3b23b3ad2d96dd70a628fcd6e2a1b1c4da6cedcd
jakobtroidl <jtroidl@g.harvard.edu>
2023-11-19 16:53:41 -0500

    use MLP as decoder

--- modules.py
+++ modules.py
@@ -5,26 +5,32 @@ import tinycudann as tcnn
 import modulation
 from sparsefeaturegrid import SparseFeatureGrid
 from edsr import EDSR
+from decoder import MLP
        
 class NVP(nn.Module):
 
     def __init__(self, out_features=3, encoding_config=None, latent_grid=None, **kwargs):
         super().__init__()
-
+        n_features = encoding_config["encoder"]["n_features"]
+        mlp_n_layers = encoding_config["network"]["n_hidden_layers"]
+        mlp_n_neurons = encoding_config["network"]["n_neurons"]
         self.encoder = EDSR(args = encoding_config["encoder"])
         self.latent_grid = latent_grid
-        self.sparse_grid = SparseFeatureGrid(level_dim=encoding_config["encoder"]["n_features"], upsample=False)
+        self.sparse_grid = SparseFeatureGrid(level_dim=n_features, upsample=False)
+        self.decoder = MLP(in_dim=n_features, out_dim=out_features, n_neurons=mlp_n_neurons, n_hidden=mlp_n_layers)
+
+
                 
-        self.net = modulation.SirenNet(
-                                    dim_in = 3, # input dimension, ex. 2d coor
-                                    dim_hidden = encoding_config["network"]["n_neurons"],       # hidden dimension
-                                    dim_out = out_features,                                     # output dimension, ex. rgb value
-                                    num_layers = encoding_config["network"]["n_hidden_layers"], # number of layers
-                                    w0_initial = 30.,                                            # different signals may require 
-                                                                                                 # different omega_0 in the first layer                                                               #  - this is a hyperparameter
-                                    )
+        # self.net = modulation.SirenNet(
+        #                             dim_in = 3, # input dimension, ex. 2d coor
+        #                             dim_hidden = encoding_config["network"]["n_neurons"],       # hidden dimension
+        #                             dim_out = out_features,                                     # output dimension, ex. rgb value
+        #                             num_layers = encoding_config["network"]["n_hidden_layers"], # number of layers
+        #                             w0_initial = 30.,                                            # different signals may require 
+        #                                                                                          # different omega_0 in the first layer                                                               #  - this is a hyperparameter
+        #                             )
                 
-        self.wrapper = modulation.SirenWrapper(self.net, latent_dim = encoding_config["encoder"]["n_features"])
+        # self.wrapper = modulation.SirenWrapper(self.net, latent_dim = encoding_config["encoder"]["n_features"])
 
     def forward(self, coords, image=None, train=True):
         if train:
@@ -37,7 +43,7 @@ class NVP(nn.Module):
         # at test time, we do not need the image but just reconstruct ouput based in coordinates
         coords = coords.unsqueeze(0)
         net_input = self.sparse_grid.compute_features(self.latent_grid, coords)
-        output = self.wrapper(coords=coords, latent=net_input)
+        output = self.decoder(net_input)
         output = output.squeeze()
         return {'model_out': output}
 
@@ -46,7 +52,7 @@ class NVP(nn.Module):
         
         self.latent_grid = self.encoder(image)
 
-        # print('features min max: ', features.min(), features.max())
+        # print('features min max: ', self.latent_grid.min(), self.latent_grid.max())
 
         net_input = self.sparse_grid.compute_features(self.latent_grid, coords)
 
@@ -80,7 +86,7 @@ class NVP(nn.Module):
         # embedding = torch.cat((spatial_embedding, motion_embedding), dim=1)
         
         # modulation latent
-        output = self.wrapper(coords=coords, latent=net_input)
+        output = self.decoder(net_input)
         output = output.squeeze()
         # output = output.reshape((b, t, 3))
 
