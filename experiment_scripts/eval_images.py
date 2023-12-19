aa23793192ad0f396224eaff6f185af888633261
jakobtroidl <jtroidl@g.harvard.edu>
2023-12-19 06:24:14 -0500

    feat: plot PSNR for base lines too

--- experiment_scripts/eval_images.py
+++ experiment_scripts/eval_images.py
@@ -14,6 +14,9 @@ from torch.utils.data import DataLoader  # noqa: E402
 import math
 from torch.nn import functional as F
 
+from loss_functions import compute_psnr
+
+
 import PIL.Image as Image
 
 command_line = ""
@@ -23,7 +26,7 @@ for arg in sys.argv:
 
 p = configargparse.ArgumentParser()
 p.add('-c', '--config', required=True,  help='Path to config file.')
-p.add_argument('--logging_root', type=str, default='./logs_nvp', help='root for logging')
+p.add_argument('--logging_root', type=str, default='./logs', help='root for logging')
 p.add_argument('--experiment_name', type=str, required=False, default="",
                help='Name of subdirectory in logging_root where summaries and checkpoints will be saved.')
 
@@ -55,20 +58,23 @@ model.load_state_dict(checkpoint['model'])
 # model.set_latent_grid(checkpoint['latent_grid'])
 
 '''Load Volume Dataset'''
-volume_dataset = dataio.ImageDataset(path_to_info=opt.dataset, train=False)
-dataloader = DataLoader(volume_dataset, shuffle=False, batch_size=1, num_workers=0)
+dataset = dataio.ImageDataset(path_to_info=opt.dataset, train=False)
+dataloader = DataLoader(dataset, shuffle=False, batch_size=1, num_workers=0)
 
 results = {}
 results_dir = os.path.join(root_path, 'results')
 os.makedirs(results_dir, exist_ok=True)
 
-psnrs = []
+result_psnrs = []
+nearest_psnrs = []
+bilinear_psnrs = []
+
 model.cuda()
 model.eval()
 
 unit_multiplier = 255.0
 
-for step, (model_input, coords, file_name) in enumerate(dataloader):
+for step, (model_input, coords, gt, file_name) in enumerate(dataloader):
 
     with torch.no_grad():
 
@@ -76,27 +82,55 @@ for step, (model_input, coords, file_name) in enumerate(dataloader):
         prediction = prediction["model_out"]
 
         side_length = int(math.sqrt(prediction.shape[1]))
-        image = prediction.squeeze().view(side_length, side_length).cpu().numpy()
-        # save image to disk as png
-        result_name = os.path.join(results_dir, "result_{}".format(file_name[0]))
-        image = Image.fromarray(np.uint8(image * unit_multiplier))
-        image.save(result_name)
 
         # export normal interpolated images
         nearest = F.interpolate(model_input, size=[side_length, side_length], mode='nearest')
         bilinear = F.interpolate(model_input, size=[side_length, side_length], mode='bilinear', align_corners=False)
 
+        ## compute PSNR and other metrics of isotropic test data is available 
+        if dataset.has_isotropic_test_data():
+            result_psnr = compute_psnr(prediction, gt)
+            result_psnrs.append(result_psnr)
+
+            nearest_psnr = compute_psnr(nearest.squeeze().view(side_length * side_length, 1), gt)
+            nearest_psnrs.append(nearest_psnr)
+
+            bilinear_psnr = compute_psnr(bilinear.squeeze().view(side_length * side_length, 1), gt)
+            bilinear_psnrs.append(bilinear_psnr)
+
+            print("Result PSNR: {}, Bilinear PSNR: {}, Nearest PSNR: {}".format(result_psnr, bilinear_psnr, nearest_psnr))
+
+        result_name = os.path.join(results_dir, "result_{}".format(file_name[0]))
+        nearest_name = os.path.join(results_dir, "nearest_{}".format(file_name[0]))
+        linear_name = os.path.join(results_dir, "bilinear_{}".format(file_name[0]))
+
+        if dataset.has_isotropic_test_data():
+            result_psnr_str = str(result_psnr).replace(".", ",")
+            result_name = os.path.join(results_dir, "result_psnr_{}_{}".format(result_psnr_str, file_name[0]))
+
+            nearest_psnr_str = str(nearest_psnr).replace(".", ",")
+            nearest_name = os.path.join(results_dir, "nearest_psnr_{}_{}".format(nearest_psnr_str, file_name[0]))
+
+            bilinear_psnr_str = str(bilinear_psnr).replace(".", ",")
+            linear_name = os.path.join(results_dir, "bilinear_psnr_{}_{}".format(bilinear_psnr_str, file_name[0]))
+
+        image = prediction.squeeze().view(side_length, side_length).cpu().numpy()
         nearest = nearest.squeeze().cpu().numpy()
         bilinear = bilinear.squeeze().cpu().numpy()
 
+        image = Image.fromarray(np.uint8(image * unit_multiplier))
+        image.save(result_name)
+
         # save image to disk as png
-        nearest_name = os.path.join(results_dir, "nearest_{}".format(file_name[0]))
         image = Image.fromarray(np.uint8(nearest * unit_multiplier))
         image.save(nearest_name)
 
-        linear_name = os.path.join(results_dir, "bilinear_{}".format(file_name[0]))
         image = Image.fromarray(np.uint8(bilinear * unit_multiplier))
         image.save(linear_name)
 
+print("-------------------------------")
+print("Average Result PSNR: {}".format(np.mean(result_psnrs)))
+print("Average Bilinear PSNR: {}".format(np.mean(bilinear_psnrs)))
+print("Average Nearest PSNR: {}".format(np.mean(nearest_psnrs)))
 print("Done!")
     
